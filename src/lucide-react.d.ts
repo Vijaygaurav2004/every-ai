823fdace0ba876795013fc10ff069563897da818
VinayReddy18 <142021280+VinnuReddy18@users.noreply.github.com>
2024-10-03 17:29:26 +0530

    deploy5

---
+++ src/lucide-react.d.ts
@@ -0,0 +1,21 @@
+declare module 'lucide-react' {
+  import { FC, SVGProps } from 'react'
+  export const Search: FC<SVGProps<SVGSVGElement>>
+  export const Home: FC<SVGProps<SVGSVGElement>>
+  export const FolderOpen: FC<SVGProps<SVGSVGElement>>
+  export const Users: FC<SVGProps<SVGSVGElement>>
+  export const Plus: FC<SVGProps<SVGSVGElement>>
+  export const Play: FC<SVGProps<SVGSVGElement>>
+  export const User: FC<SVGProps<SVGSVGElement>>
+  export const ChevronDown: FC<SVGProps<SVGSVGElement>>
+  export const MessageSquare: FC<SVGProps<SVGSVGElement>>
+  export const Image: FC<SVGProps<SVGSVGElement>>
+  export const Code: FC<SVGProps<SVGSVGElement>>
+  export const Music: FC<SVGProps<SVGSVGElement>>
+  export const Video: FC<SVGProps<SVGSVGElement>>
+  export const FileText: FC<SVGProps<SVGSVGElement>>
+  export const MoreHorizontal: FC<SVGProps<SVGSVGElement>>
+  export const Send: FC<SVGProps<SVGSVGElement>>
+  export const ArrowLeft: FC<SVGProps<SVGSVGElement>>
+  export const Download: FC<SVGProps<SVGSVGElement>>
+}
